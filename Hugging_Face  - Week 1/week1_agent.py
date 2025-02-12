46705ab36bf5fe57ab9f3978d376b8b080b04810
LouisZCode <lgzg90@hotmail.com>
2025-02-12 07:58:59 +0100

    add git ignore

--- Hugging_Face  - Week 1/week1_agent.py
+++ Hugging_Face  - Week 1/week1_agent.py
@@ -2,11 +2,11 @@ import os
 from huggingface_hub import InferenceClient
 
 #Permits to use an open LLM, to check later how to get my local or Claude in this:
-HF_TOKEN = os.environ.get("HF_TOKEN")
+HF_TOKEN = os.getenv("HF_TOKEN")
 client = InferenceClient("meta-llama/Llama-3.2-3B-Instruct")
 
 
-my_prompt = "what version are you?"
+my_prompt = "wath is the capital of France?"
 prompt=f"<|begin_of_text|><|start_header_id|>user<|end_header_id|>{my_prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
 
 output = client.text_generation(
